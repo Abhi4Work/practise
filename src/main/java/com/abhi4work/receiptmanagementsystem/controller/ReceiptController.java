1906600541bb01b8137bf88004a91ff1bb76a101
Abhigyan <abhigyannanda.work@gmail.com>
2023-01-02 00:34:13 +0530

    adding cross origin

--- src/main/java/com/abhi4work/receiptmanagementsystem/controller/ReceiptController.java
+++ src/main/java/com/abhi4work/receiptmanagementsystem/controller/ReceiptController.java
@@ -10,12 +10,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class ReceiptController implements ReceiptAPI
 {
 	private static final Logger logger = LogManager.getLogger(ReceiptController.class);
